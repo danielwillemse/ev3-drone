afe36d96b880c1262c7952543f0a215418b9a835
Daniel Willemse <daniel.willemse@bettyblocks.com>
2017-02-23 00:50:03 +0100

    Move device connectivity status to Ev3.Utils

--- lib/ev3/device.ex
+++ lib/ev3/device.ex
@@ -2,4 +2,13 @@ defmodule Ev3.Device do
   defmodule InvalidCommandError do
     defexception message: "Invalid command for ev3 device"
   end
+
+  def connected_devices(type) do
+    Ev3.Util.ls(type)
+  end
+
+  def connected?(type, name) do
+    connected_devices(type)
+    |> Enum.any?(fn(f) -> f == name end)
+  end
 end
